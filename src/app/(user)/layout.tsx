9a35746c58a3d286723613ade33a375a6f1bdf6e
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-07-28 00:05:46 +0300

    layout

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -4,7 +4,7 @@ import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
 import prisma from "@/lib/prisma"
 import { getUser } from "@/lib/actions"
-import SettingsPage from "./page"
+import SettingsPage from "./settings/page"
 
 const layout = async () => {
   const session = await getServerSession(authOptions)
