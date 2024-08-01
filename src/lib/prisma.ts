db9d275c6b79d95e2233d9703d1fc8b60b59867a
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-08-01 17:13:04 +0300

    Version 0.9 prisma error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from '@prisma/extension-accelerate'
-const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate())
-};
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient().$extends(withAccelerate())
+// };
+
+// declare global {
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
+
+// const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
